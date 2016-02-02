4da07c249aea3e4b94cf15c4880e6c73b20204e7
Steve Savio <steve@bettersms.net>
2016-02-02 18:51:35 +0300

    Initial commit

---
+++ LoanManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoanManager.Startup))]
+namespace LoanManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
