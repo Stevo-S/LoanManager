baaab6ff131ff8d1f101f2ae4e36990d349f73f2
Steve Savio <steve@bettersms.net>
2016-02-04 19:09:15 +0300

    Fix Asset Datetime field issue

--- LoanManager/Controllers/AssetsController.cs
+++ LoanManager/Controllers/AssetsController.cs
@@ -83,7 +83,7 @@ namespace LoanManager.Controllers
         // POST: Assets/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost];
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Description,Value,LogBookId,BorrowerId,CreatedAt,ModifiedAt")] Asset asset)
         {
