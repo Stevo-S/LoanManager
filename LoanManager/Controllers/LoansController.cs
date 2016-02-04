c46b4ca9acd331d5700bb9dfee891fd1fed41f85
Steve Savio <steve@bettersms.net>
2016-02-04 19:21:55 +0300

    Scaffold Loans views and controller

---
+++ LoanManager/Controllers/LoansController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LoanManager.Models;
+
+namespace LoanManager.Controllers
+{
+    public class LoansController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Loans
+        public ActionResult Index()
+        {
+            var loans = db.Loans.Include(l => l.Asset);
+            return View(loans.ToList());
+        }
+
+        // GET: Loans/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Loan loan = db.Loans.Find(id);
+            if (loan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loan);
+        }
+
+        // GET: Loans/Create
+        public ActionResult Create()
+        {
+            ViewBag.AssetId = new SelectList(db.Assets, "Id", "Description");
+            return View();
+        }
+
+        // POST: Loans/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Principal,Interest,Date,Cleared,AssetId")] Loan loan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Loans.Add(loan);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AssetId = new SelectList(db.Assets, "Id", "Description", loan.AssetId);
+            return View(loan);
+        }
+
+        // GET: Loans/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Loan loan = db.Loans.Find(id);
+            if (loan == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AssetId = new SelectList(db.Assets, "Id", "Description", loan.AssetId);
+            return View(loan);
+        }
+
+        // POST: Loans/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Principal,Interest,Date,Cleared,AssetId")] Loan loan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(loan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AssetId = new SelectList(db.Assets, "Id", "Description", loan.AssetId);
+            return View(loan);
+        }
+
+        // GET: Loans/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Loan loan = db.Loans.Find(id);
+            if (loan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loan);
+        }
+
+        // POST: Loans/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Loan loan = db.Loans.Find(id);
+            db.Loans.Remove(loan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
