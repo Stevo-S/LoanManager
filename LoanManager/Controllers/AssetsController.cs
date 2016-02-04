6170656c26a823980e17335f8cac28856282b9c6
Steve Savio <steve@bettersms.net>
2016-02-04 19:11:00 +0300

    Fix syntax error

--- LoanManager/Controllers/AssetsController.cs
+++ LoanManager/Controllers/AssetsController.cs
@@ -83,12 +83,11 @@ namespace LoanManager.Controllers
         // POST: Assets/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost];
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Description,Value,LogBookId,BorrowerId,CreatedAt,ModifiedAt")] Asset asset)
         {
             asset.ModifiedAt = DateTime.Now;
-
             if (ModelState.IsValid)
             {
                 db.Entry(asset).State = EntityState.Modified;
