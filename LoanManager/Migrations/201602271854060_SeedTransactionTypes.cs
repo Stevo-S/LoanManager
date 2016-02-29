ada9abc70eb9ffb9236343c335484829a9a8df88
Steve Savio <steve@bettersms.net>
2016-02-29 20:31:53 +0300

    Seed basic transaction types

---
+++ LoanManager/Migrations/201602271854060_SeedTransactionTypes.cs
@@ -0,0 +1,16 @@
+namespace LoanManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedTransactionTypes : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
